67c2345456642de32e10d792f4dee22ff60f16cb
ZenithK <34740476+ZenithK@users.noreply.github.com>
2021-05-19 12:46:20 +0900

    Youchu-Server v2

---
+++ src/test/java/link/youchu/youchuserver/repository/UserRepositoryTest.java
@@ -0,0 +1,38 @@
+package link.youchu.youchuserver.repository;
+
+import link.youchu.youchuserver.Dto.UserDto;
+import link.youchu.youchuserver.Dto.UserSearchCondition;
+import link.youchu.youchuserver.domain.Users;
+import link.youchu.youchuserver.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class UserRepositoryTest {
+
+    UserService userService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    public void basicTest(){
+        UserSearchCondition condition = new UserSearchCondition();
+        condition.setUser_id(1L);
+
+
+        System.out.println(userRepository.getUserData(condition));
+    }
+
+}
